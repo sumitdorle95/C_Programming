acc4efa4c693774836d07da438ac89e8fd278bb5
sumitdorle95 <sumitdorle95@gmail.com>
2025-11-08 12:57:21 +0530

    Add Assignment Solutions (Q1-Q5)

---
+++ Assignments/Assignment_01/PROG1_2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void display()
+{
+    int i =0;
+    for (i=1;i<=5;i++)
+    {
+        printf("MARVELLOUS\n");
+    }
+}
+
+int main ()
+{
+    display();
+    return 0;
+}
---
+++ Assignments/Assignment_01/PROG1_3.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void display()
+{
+    int i =5;
+    
+    while (i>0)
+
+    {
+        printf("%d\t",i);
+        i--;
+    }
+}
+
+int main ()
+{
+    display();
+    return 0;
+}
