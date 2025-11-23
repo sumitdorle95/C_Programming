7c0e0bc8ead0e0850d3798e6750f8c914167824d
sumitdorle95 <sumitdorle95@gmail.com>
2025-11-23 13:09:51 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/PROG31_1.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+ void display(int iRow, int iCol)
+{
+    int i,j=0;
+
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            if((i==j)||(i>j))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("");
+            }
+        }
+        printf("\n");
+
+    }
+        
+}
+ 
+
+ int main()
+ {
+    int iValue,iValue2=0;
+
+    printf("enter the no of rows and columns:");
+    scanf("%d%d",&iValue,&iValue2);
+    display(iValue,iValue2);
+
+    return 0;
+ }
